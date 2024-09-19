18a6f359e7cd732649d70f57159056291867c608
ReemHassan578 <reemhassan5523@gmail.com>
2024-09-19 03:20:40 +0300

    add Google Map to App

--- PlacesApp/places/lib/screens/place_details.dart
+++ PlacesApp/places/lib/screens/place_details.dart
@@ -4,22 +4,55 @@ import 'package:flutter/material.dart';
 
 import '../models/place.dart';
 import '../providers/list_provider.dart';
+import 'map_screen.dart';
 
 class PlaceDetails extends StatelessWidget {
   final Place place;
-  
-  const PlaceDetails({super.key,required this.place});
+
+  const PlaceDetails({super.key, required this.place});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title:  Text(place.name),
+        title: Text(place.name),
       ),
-      body: Center(
-        child :Text(place.id),
-      ) ,
+      body: Stack(children: [
+        Image.file(
+          place.image,
+          fit: BoxFit.fill,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Column(
+            children: [
+              InkWell(onTap:(){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => MapScreen(placeLocation: place.placeLocation ,isSelecting: false,),));
+              },
+                  child: CircleAvatar(
+                radius: 40,
+                backgroundImage: NetworkImage(place.placeLocation.mapImage),
+              )),
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black54],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter),
+                ),
+                child: Text(place.placeLocation.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleSmall),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
--- PlacesApp/places/lib/widgets/places_list.dart
+++ PlacesApp/places/lib/widgets/places_list.dart
@@ -4,23 +4,31 @@ import '../models/place.dart';
 
 class PlacesList extends StatelessWidget {
   final List<Place> list;
-  final void Function(BuildContext cntx,Place nameP) clickPlace;
-  const PlacesList(this.list,this.clickPlace, {super.key});
+  final void Function(BuildContext cntx, Place nameP) clickPlace;
+  const PlacesList(this.list, this.clickPlace, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    if(list.isEmpty){
+    if (list.isEmpty) {
       return const Center(
         child: Text('No places added yet '),
       );
     }
     return ListView.builder(
         itemCount: list.length,
-        itemBuilder: (cntx, index) => ListTile( onTap: () {
-          clickPlace(context,list[index]);
-        },
-              title: Text(list[index].name,style: Theme.of(context).textTheme.titleLarge,
-            ),
+        itemBuilder: (cntx, index) => ListTile(
+              leading: CircleAvatar(
+                backgroundImage: FileImage(list[index].image),
+              ),
+              onTap: () {
+                clickPlace(context, list[index]);
+              },
+              subtitle: Text(list[index].placeLocation.address, maxLines: 2, 
+                  style: Theme.of(context).textTheme.bodySmall),
+              title: Text(
+                list[index].name,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ));
   }
 }
